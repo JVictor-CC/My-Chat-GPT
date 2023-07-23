ff1b9fa9ac4d9b6787a46cbd72ed9332e1b218b9
JVictor-CC <jvcunha1221@hotmail.com>
2023-07-23 20:48:22 -0300

    Add service layer for backend communication

---
+++ client/src/services/auth.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { toast } from "react-toastify"
+
+export async function loginUser(email, password) {
+    try {
+        const response = await axios.post('http://localhost:8080/login', { 'email':email, 'password':password });
+        if(response.data.success) {
+            toast.success(response.data.message)
+            return response.data.token
+        }else {
+            toast.error(response.data.message)
+            return false
+        }
+        
+    } catch (error) {
+        toast.error(error.message)
+        return false
+    }
+}
+
+export async function registerUser(username, email, password) {
+    try {
+        const response = await axios.post('http://localhost:8080/register', {"username":username, "email":email, "password":password });
+        console.log(response.data.success)
+        if(response.data.success) {
+            toast.success(response.data.message)
+            return true
+        }else {
+            toast.error(response.data.message)
+            return false
+        }
+    } catch (error) {
+        toast.error(error.message)
+        return false
+    }
+  }
---
+++ client/src/services/chat.js
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import { toast } from "react-toastify"
+
+export async function createChat(token,chatName) {
+    try {
+        const response = await axios.post('http://localhost:8080/createChat',
+        {
+            chatName: chatName
+        },
+        {
+            headers: {
+                Authorization: 'Bearer ' + token 
+            }
+        })
+        return response.data.chats  
+        
+    } catch (error) {
+        toast.error("Couldn't load the user chats!")
+        return false
+    }
+}
+
+export async function loadChats(token) {
+    try {
+        const response = await axios.get('http://localhost:8080/loadChats', {
+            headers: {
+                Authorization: 'Bearer ' + token 
+            }
+        })
+        return response.data.chats  
+        
+    } catch (error) {
+        toast.error("Couldn't load the user chats!")
+        return false
+    }
+}
+
+export async function deleteAllChats(token) {
+    try {
+        const response = await axios.put('http://localhost:8080/deleteAllChats', {
+            headers: {
+                Authorization: 'Bearer ' + token 
+            }
+        })
+        toast.success(response.data.message)
+        return true 
+        
+    } catch (error) {
+        toast.error("Couldn't delete the user chats!")
+        return false
+    }
+}
+
+export async function sendMessage(token, message, selectedChat) {
+    try {
+        console.log(selectedChat)
+        const response = await axios.post('http://localhost:8080/sendMessage', 
+        {
+            message: message.text,
+            chatIndex: selectedChat
+        },
+        {
+            headers: {
+                Authorization: 'Bearer ' + token 
+            }
+        })
+        return response  
+        
+    } catch (error) {
+        toast.error("Couldn't save the user chat!")
+        return false
+    }
+}
